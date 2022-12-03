854cc839e40d2a9021676594c2e87075b3886e52
Ambuj Pawar <your_email@abc.example>
2022-12-03 11:22:28 +0100

    ADD: Utility functions to parse AoC input

---
+++ utils/read_input.py
@@ -0,0 +1,12 @@
+"""
+Utilities for parsing input files from Advent of Code.
+"""
+from typing import Any, List
+
+
+def read_input(path: str) -> List[Any]:
+    """
+    Read the input file at the given path and return a list of the lines.
+    """
+    input_strings = open(path).readlines()
+    return [line.strip() for line in input_strings]
